51fc342124dd9119b86edf731a26459d6610900a
htu6n7yi <jcavalcanti008@gmail.com>
2025-03-02 20:23:38 -0300

    components dinamycs propertyes

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+
+  @Input ()
+  photoCover: string = "https://th.bing.com/th/id/OIP.Isy8LFQTDSQVRIIBetscIgHaFj?w=238&h=180&c=7&r=0&o=5&pid=1.7";
+  @Input ()
+  cardTitle: string = "SAIBA TUDO SOBRE DAYSE JHONSON";
+
+  constructor() {}
+
+  ngOnInit() {}
 
 }
